0fd7a3f279f86063a6c321117880d812611e4984
Matthew Cramblett <Cramblett.10@osu.edu>
2016-11-29 19:18:40 -0500

    pedestrians now spawn to unique locations, and control points have been made

--- Assets/Pedestrian.cs
+++ Assets/Pedestrian.cs
@@ -1,113 +1,107 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Pedestrian : MonoBehaviour {
 
-	public string orientation;
-	public string position;
-	public bool wishToCross = false;
-	public bool moving = true;
-	public GameObject body;
-	//sidewalk position based on grid of blocks
-	//5 6 7 8
-	//1 2 3 4
-	//Each block has left and/or right and a short side
-	//Also an orientation (left/right or up/down)
+	public int numberOfGuys = 8;
+	int guyNum;
 
-	//1 Right: (-10.75 , .15 , -19.75) - (-10.75 , .15 , -0.75)
-	//1 Short: (-10.75 , .15 , -0.75) - (-19.75 , .15f , -0.75)
-	//5 Short: (-19.75 , .15 , 0.75) - (-10.75 , .15 , 0.75)
-	//5 Right: (-10.75 , .15 , 0.75) - (-10.75 , .15 , 19.75)
-	//2 Left: (-9.25 , .15 , -19.75) - (-9.25 , .15 , -0.75)
-	//2 Short: (-9.25 , .15 , -0.75) - (-.75 , .15 , -0.75)
-	//2 Right: (-0.75 , .15 , -0.75) - (-0.75, .15 , -19.75)
-	//6 Left: (-9.25 , .15, 0.75) - (-9.25 , .15 , 19.75)
-	//6 Short: (-9.25 , .15 , 0.75) - (-0.75 , .15 , 0.75)
-	//6 Right: (-0.75 , .15 , 0.75) - (-0.75 , .15 , 19.75)
-	//7 Left: (0.75 , .15 , 19.75) - (0.75 , .15 , 0.75)
-	//7 Short: (0.75 , .15 , 0.75) - (9.25 , .15 , 0.75)
-	//7 Right: (9.25 , .15 , 0.75) - (9.25 , .15 , 19.75)
-	//3 Left: (0.75 , .15 , -0.75) - (0.75 , .15 , -19.75)
-	//3 Short: (0.75 , .15 , -0.75) - (9.25 , .15 , -0.75) 
-	//3 Right: (9.25 , .15 , -19.75) - (9.25 , .15 , -0.75)
-	//4 Left: (10.75 , .15 , -0.75) - (-19.75 , .15 , 19.75)
-	//4 Short: (10.75 , .15 , -0.75) - (19.75 , .15 , -0.75)
-	//8 Short: (10.75 , .15, 0.75) - (19.75 , .15, -0.75)
-	//8 Left: (10.75 , .15 , 0.75) - (10.75 , .15 , -19.75)
-
-	string setInitialOrientation(string op1 , string op2){
-		string orient = "";
-		int randorient = Random.Range (1, 3);
-		switch (randorient) {
-		case 1:
-			orient = op1;
-			break;
-		case 2:
-			orient = op2;
-			break;
-		}
-		return orient;
-	}
-
-	public Pedestrian(){
-		this.body = GameObject.CreatePrimitive(PrimitiveType.Cube); // used for testing positions: positions are good 
-		body.AddComponent<Rigidbody> ();
-		int randpos = Random.Range (0, 5);
-		this.body.transform.localScale = new Vector3 (.5f, .5f, .5f);
-
-		//generation locations are in front of some buildings that approach the road
-		switch (randpos) {
-		case 0:
-			//building on 3 left 
-			this.orientation = setInitialOrientation("up","down");
-			this.position = "3left";
-			this.body.transform.position = new Vector3 (0.75f, .15f, -14.25f);
-			break;
-		case 1:
-			//building on 2 right towards bottom
-			this.orientation = setInitialOrientation("up","down");
-			this.position = "2right";
-			this.body.transform.position = new Vector3 (-0.75f, .15f, -14.5f);
-			break;
-		case 2:
-			//building on 3 right
-			this.orientation = setInitialOrientation("up","down");
-			this.position = "3right";
-			this.body.transform.position = new Vector3 (9.25f, .15f, -17.5f);
-			break;
-		case 3:
-			//building on 7 left
-			this.orientation = setInitialOrientation("up","down");
-			this.position = "7left";
-			this.body.transform.position = new Vector3 (0.75f, .15f, 3.75f);
-			break;
-		case 4:
-			//building on 6 short
-			this.orientation = setInitialOrientation("left","right");
-			this.position = "6short";
-			this.body.transform.position = new Vector3 (-7.5f, .15f, 0.75f);
-			break;
-		}
-	}
-
-	/// <summary>
-	/// Turn at corner
-	/// </summary>
-	void Turn(){
-	}
-
-	/// <summary>
-	/// Checks if safe to cross or not
-	/// </summary>
-	bool Cross(){
-		return false;
-	}
-
-	// Use this for initialization
-	void Start () {
+	//sidewalk position based on grid of blocks:
+	//	   end1   end2    end3
+	//	    ||	   ||	   ||
+	// a   b c    d e     f g    h
+	//   --	| --   ||  --- || -- 
+	// i   j k    l m     n o    p
+	//		||	   ||	   ||
+	//     end4   end5    end6	
 	
+	//orientation:
+	// z ^
+	// x >
+
+	//control points:
+	Vector3 a = new Vector3(-20f, 0f, .75f); //endpoint
+	Vector3 b = new Vector3(-10.75f, 0f, .75f);
+	Vector3 c = new Vector3(-9.35f, 0f, .75f);
+	Vector3 d = new Vector3(-0.75f, 0f, 0.75f);
+	Vector3 e = new Vector3(0.75f, 0f, 0.75f);
+	Vector3 f = new Vector3(9.3f, 0f, 0.75f);
+	Vector3 g = new Vector3(10.75f, 0f, 0.75f);
+	Vector3 h = new Vector3(20f, 0f, 0.75f); //endpoint
+
+	Vector3 i = new Vector3(-20f, 0f, -0.75f); //endpoint
+	Vector3 j = new Vector3(-10.75f, 0f, -0.75f);
+	Vector3 k = new Vector3(-9.35f, 0f, -0.75f);
+	Vector3 l = new Vector3(-0.75f, 0f, -0.75f);
+	Vector3 m = new Vector3(0.75f, 0f, -0.75f);
+	Vector3 n = new Vector3(9.3f, 0f, -0.75f);
+	Vector3 o = new Vector3(10.75f, 0f, -0.75f);
+	Vector3 p = new Vector3(20f, 0f, -0.75f); //endpoint
+
+	//each 'end' corresponds to the exit points in line with
+	//the letter of that intersection 
+	Vector3 endOneB = new Vector3(-10.75f, 0f, 20f);
+	Vector3 endOneC = new Vector3(-9.35f, 0f, 20f);
+	Vector3 endTwoD = new Vector3(-0.75f, 0f, 20f);
+	Vector3 endTwoE = new Vector3(0.75f ,0f, 20f);
+	Vector3 endThreeF = new Vector3(9.3f, 0f, 20f);
+	Vector3 endThreeG = new Vector3(10.75f, 0f, 20f);
+
+	Vector3 endFourJ = new Vector3(-10.75f, 0f, -20f);
+	Vector3 endFourK = new Vector3(-9.35f, 0f, -20f);
+	Vector3 endFiveL = new Vector3(-0.75f, 0f, -20f);
+	Vector3 endFiveM = new Vector3(0.75f, 0f, -20f);
+	Vector3 endSixN = new Vector3(9.3f, 0f, -20f);
+	Vector3 endSixO = new Vector3(10.75f, 0f, -20f);
+
+	List<Vector3> endpoints = new List<Vector3>();
+	List<GameObject> guyList = new List<GameObject>();
+
+	void Start(){
+		endpoints.Add(a);
+		endpoints.Add(h);
+		endpoints.Add(p);
+		endpoints.Add(endOneB);
+		endpoints.Add(endOneC);
+		endpoints.Add(endTwoD);
+		endpoints.Add(endTwoE);
+		endpoints.Add(endThreeF);
+		endpoints.Add(endThreeG);
+		endpoints.Add(endFourK);
+		endpoints.Add(endFourK);
+		endpoints.Add(endFiveL);
+		endpoints.Add(endSixN);
+		endpoints.Add(endSixO);
+
+		//collect list of other guys
+		for (int z = 1; z <= numberOfGuys; z++) {
+			string thisnum = this.ToString ();
+			thisnum = thisnum.Substring (3,1);
+			if (i.ToString () != thisnum) {
+				guyList.Add (GameObject.Find ("guy" + z.ToString ()));
+			} else {
+				guyNum = z;
+			}
+		}
+
+		int randpos = Random.Range(0, endpoints.Count);
+		transform.position = endpoints[randpos];
+
+		//assign new position if another guy is already there
+		foreach (GameObject guy in guyList){
+			if(guy.transform.position == transform.position){
+				int randp = Random.Range(0, endpoints.Count);
+				transform.position = endpoints[randp];
+			}
+		}
+
 	}
-	
+
+	void AssignTarget(){
+		
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
